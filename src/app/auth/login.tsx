dc8b06ed49b1dbbbb2bb2cd25f8cd096ac4851bb
ebrardev <16155052@mersin.edu.tr>
2024-05-30 01:32:41 +0300

    someting about us

---
+++ src/app/auth/login.tsx
@@ -0,0 +1,10 @@
+import { Text } from "react-native"
+import { View } from "react-native"
+
+export default function LoginScreen() {
+    return (
+        <View>
+       <Text></Text>
+        </View>
+    )
+}

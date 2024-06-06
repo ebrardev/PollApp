a9936a64ea4220a8bbcd8c92c1da1a9c47b1faac
ebrardev <16155052@mersin.edu.tr>
2024-06-06 14:00:26 +0300

    protect auth

---
+++ src/app/(auth)/_layout.tsx
@@ -0,0 +1,14 @@
+import { Redirect, Slot } from "expo-router";
+import { useAuth } from "../../providers/AuthProvider";
+
+export default function AuthLayout(){
+
+    const {user} = useAuth()
+
+    if(user) {
+        return <Redirect href="/profile" />
+    }
+ 
+
+    return <Slot/>
+}
